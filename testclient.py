02ed47023ed4aae7b700239615a08ad776e3771f
mjezersky <mjezersky@users.noreply.github.com>
2017-12-18 21:16:51 +0100

    c

---
+++ testclient.py
@@ -0,0 +1,19 @@
+import facerecprotocol as frp
+
+p = frp.FacerecProtocol()
+p.connect("10.0.75.2", 9000)
+
+
+f = open("D:\\skola\\DP - cam\\testfiles\\test1.jpg", "rb")
+data1 = f.read()
+f.close()
+
+f = open("D:\\skola\\DP - cam\\testfiles\\test2.jpg", "rb")
+data2 = f.read()
+f.close()
+
+p.send(data1)
+p.send(data2)
+resp = p.recv()
+print resp
+raw_input()
